516d7698828a8e64310009929d4815aa8ce16a3d
Rusisariev <76563518+Rusisariev@users.noreply.github.com>
2021-09-02 11:05:23 +0600

    game page update

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -1,13 +1,18 @@
 import styles from "../../sass/header.module.sass";
 import Page from "./PageLink";
+import React from 'react';
+
 
 
 const Header = () => {
+
     return (
-        <div className={styles.navbar}>
-            <Page text={'Главная'} href={'/'} />
-            <Page text={'Игра'} href={'/games'} />
-        </div>
+        <>
+            <div className={styles.navbar}>
+                <Page text={'Главная'} href={'/'}/>
+                <Page text={'Игра'} href={'/games'}/>
+            </div>
+        </>
     )
 }
 
--- pages/games.js
+++ pages/games.js
@@ -1,13 +1,17 @@
-import styles from "../styles/Home.module.css";
-import Page from "./components/PageLink";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import sass from "../sass/games.module.sass"
 
 
 export default function Games() {
     return <>
-        <Header />
-        <h1>page game</h1>
-        <Footer />
-        </>
-        }
+        <Header/>
+        <div className={sass.container}>
+
+            <h1 className={sass.areYoufoodie}>ты пидор</h1>
+
+        </div>
+        <Footer/>
+    </>
+}
+
